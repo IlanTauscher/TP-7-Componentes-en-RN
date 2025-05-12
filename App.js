3e239027fe3547cdc16a8fa759995e31efcea179
Android Studio <devandroid@ort>
2025-05-12 09:07:11 -0300

    avances con el fondo y texto

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, ImageBackground, Text} from 'react-native';
 
 export default function App() {
+  const fondo = require('./assets/Hollow.jpg')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="inverted"/>
+        <ImageBackground source={fondo} resizeMode="cover" style={styles.background}>
+          <Text style={styles.title}>Hollow Knight</Text>
+        </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  }, 
+  background: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Georgia',
+    fontSize: 36,
+    color: 'white',
+    textAlign: 'center',
+    textShadowColor: '#999',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 3,
+    letterSpacing: 2,
   },
 });
